1a5b5c7f8592c6b88ba3fe078f9fb74857dbb88a
Yusin <abcd51018@gmail.com>
2022-02-09 02:16:24 +0800

    2

--- lineChatbot/urls.py
+++ lineChatbot/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import Settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
