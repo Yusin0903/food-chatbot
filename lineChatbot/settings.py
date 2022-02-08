e76c0813afb16be5829d639a1cd85cbad1009bc1
Yusin <abcd51018@gmail.com>
2022-02-09 02:05:06 +0800

    2

--- lineChatbot/settings.py
+++ lineChatbot/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lineChatbot'
+    'django_chatbot'
     # 'django_chatbot.apps.DjangoChatbotConfig',
 ]
 
