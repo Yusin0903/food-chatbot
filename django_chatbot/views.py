4986041b8df0be673b09e618e10997b6447be48e
Yusin <abcd51018@gmail.com>
2022-02-10 13:07:19 +0800

    圖片增加

--- django_chatbot/views.py
+++ django_chatbot/views.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import *
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -36,17 +36,35 @@ def callback(request):
                 elif "小小兵" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
-                        TextSendMessage("請叫我小冰冰")
+                        TextSendMessage("請叫我小冰冰www")
                     )
                 elif "學姊" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
-                        TextSendMessage("不要叫我學姊，叫我冰冰姐")
+                        TextSendMessage("不要叫我學姊，叫我冰冰姐www")
                     )
+                    
+                    line_bot_api.reply_message(
+                        event.reply_token, 
+                        ImageSendMessage(
+                            original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
+                            preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
+                        )
+                        
+                    )
+                        
                 elif "學姐" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
                         TextSendMessage("不要叫我學姐，叫我冰冰姐")
+                    )
+                    line_bot_api.reply_message(
+                        event.reply_token, 
+                        ImageSendMessage(
+                            original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
+                            preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
+                        )
+                        
                     )
 
 
@@ -57,5 +75,5 @@ def callback(request):
 
 
 def find_dinner(event):
-    return "你好我是冰冰"
+    return "你好我是冰冰www"
     
