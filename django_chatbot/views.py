1db7f8a54e1727d548280775280b6b6d7b12eb35
Yusin <abcd51018@gmail.com>
2022-02-10 13:10:13 +0800

    圖片新增

--- django_chatbot/views.py
+++ django_chatbot/views.py
@@ -41,31 +41,25 @@ def callback(request):
                 elif "學姊" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
-                        TextSendMessage("不要叫我學姊，叫我冰冰姐www")
-                    )
-                    
-                    line_bot_api.reply_message(
-                        event.reply_token, 
+                        TextSendMessage("不要叫我學姊，叫我冰冰姐www"),
                         ImageSendMessage(
                             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
                             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
                         )
-                        
                     )
+                    
+                    
                         
                 elif "學姐" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
-                        TextSendMessage("不要叫我學姐，叫我冰冰姐")
-                    )
-                    line_bot_api.reply_message(
-                        event.reply_token, 
+                        TextSendMessage("不要叫我學姐，叫我冰冰姐"),
                         ImageSendMessage(
                             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
                             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
                         )
-                        
                     )
+                   
 
 
 
