5242df0ef618a682fc5815f578b369232b5830bc
Yusin <abcd51018@gmail.com>
2022-02-10 13:23:06 +0800

    用陣列來回覆多條訊息

--- django_chatbot/views.py
+++ django_chatbot/views.py
@@ -39,30 +39,29 @@ def callback(request):
                         TextSendMessage("請叫我小冰冰www")
                     )
                 elif "學姊" in event.message.text:
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TextSendMessage("不要叫我學姊，叫我冰冰姐www"),
-                    )
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        ImageSendMessage(
+                    reply_arr =[]
+                    reply_arr.append(TextSendMessage("不要叫我學姊，叫我冰冰姊"))
+                    reply_arr.append(ImageSendMessage(
                             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
                             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
-                        )
+                        ))
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        reply_arr
                     )
                         
                 elif "學姐" in event.message.text:
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TextSendMessage("不要叫我學姐，叫我冰冰姐"),
-                    )
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        ImageSendMessage(
+                    reply_arr =[]
+                    reply_arr.append(TextSendMessage("不要叫我學姐，叫我冰冰姐"))
+                    reply_arr.append(ImageSendMessage(
                             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
                             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
-                        )
+                        ))
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        reply_arr
                     )
+                    
                 elif "阿冰" in event.message.text:
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
