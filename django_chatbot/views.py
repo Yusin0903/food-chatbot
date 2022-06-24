8e5733f821cf05e7dbc06e0d21c165ad7626468f
Yusin <abcd51018@gmail.com>
2022-06-24 12:50:32 +0800

    update

--- django_chatbot/views.py
+++ django_chatbot/views.py
@@ -10,6 +10,8 @@ from django.conf import settings
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import *
+import sys
+sys.path.append("/Users/zyusin/side_project/lineChatbot/django_chatbot")
 import crawl
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -47,48 +49,6 @@ def callback(request):
                         event.reply_token,
                         TextMessage(text = value.scrape())
                     )
-
-                # elif "小小兵" in event.message.text:
-                #     line_bot_api.reply_message(  # 回復傳入的訊息文字
-                #         event.reply_token,
-                #         TextSendMessage("請叫我小冰冰www")
-                #     )
-                # elif "學姊" in event.message.text:
-                #     reply_arr =[]
-                #     reply_arr.append(TextSendMessage("不要叫我學姊，叫我冰冰姊"))
-                #     reply_arr.append(ImageSendMessage(
-                #             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
-                #             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
-                #         ))
-                #     line_bot_api.reply_message(  # 回復傳入的訊息文字
-                #         event.reply_token,
-                #         reply_arr
-                #     )
-                        
-                # elif "學姐" in event.message.text:
-                #     reply_arr =[]
-                #     reply_arr.append(TextSendMessage("不要叫我學姐，叫我冰冰姐"))
-                #     reply_arr.append(ImageSendMessage(
-                #             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
-                #             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
-                #         ))
-                #     line_bot_api.reply_message(  # 回復傳入的訊息文字
-                #         event.reply_token,
-                #         reply_arr
-                #     )
-                    
-                # elif "阿冰" in event.message.text:
-                #     line_bot_api.reply_message(  # 回復傳入的訊息文字
-                #         event.reply_token,
-                #         ImageSendMessage(
-                #             original_content_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg",
-                #             preview_image_url="https://img.ltn.com.tw/Upload/ent/page/800/2021/02/25/phpXzdxxD.jpg"
-                #         )
-                #     )
-                   
-
-
-
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
