ccd607313daa2e9c402179e3d787d058e3e5ef0c
Yusin <abcd51018@gmail.com>
2022-06-24 12:40:10 +0800

    update

--- django_chatbot/views.py
+++ django_chatbot/views.py
@@ -34,12 +34,12 @@ def callback(request):
             if isinstance(event, MessageEvent):# 如果有訊息事件
                 if "冰冰" in event.message.text:
                     reply_arr =[]
-                    reply_arr.append(AudioSendMessage(original_content_url = "https://dl.dropbox.com/s/6m5mjnybykpevlv/%E5%86%B0%E5%86%B0.mp3?dl=0", duration=2000))
+                    # reply_arr.append(AudioSendMessage(original_content_url = "https://dl.dropbox.com/s/6m5mjnybykpevlv/%E5%86%B0%E5%86%B0.mp3?dl=0", duration=2000))
                     reply_arr.append(TextMessage("您好我是冰冰，請輸入您要選擇吃飯的城市以及地區 例如：台南市,台南市北區"))
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
                         # TextSendMessage(text=find_dinner(event.message.text))
-                        reply_arr
+                        TextMessage("您好我是冰冰，請輸入您要選擇吃飯的城市以及地區 例如：台南市,台南市北區")
                     )
                 if "台南市" in event.message.text:
                     reply_arr = []
