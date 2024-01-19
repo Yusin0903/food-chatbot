f5e11df3ee06d17260be13e44446417b60c9668b
Yusin <34501891+yusin0903@users.noreply.github.com>
2024-01-19 13:35:38 +0800

    Update settings.py

--- lineChatbot/settings.py
+++ lineChatbot/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$5!%hbcc^2)+g=#u)&kwi2yb3_en&-1azcxd(vuy4d9-7ikk95'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
